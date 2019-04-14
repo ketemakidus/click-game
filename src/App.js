9c5dcf6e6622b1dad0306309a8d07f8e442b2216
Kidus <Ketemakidus009@gmail.com>
2019-04-14 19:39:11 -0400

    friends array

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ import Title from "./components/Title";
 import friends from "./friends.json";
 import Nav from "./components/Nav";
 
-function randomFriends(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
 class App extends Component {
   state = {
     friends,
@@ -40,8 +33,6 @@ class App extends Component {
       </Wrapper>
     );
   }
-
-
 }
 
 export default App;
