1c0c0019cb30dcc777a63eb7584057f67086189e
Kidus <Ketemakidus009@gmail.com>
2019-04-14 19:35:27 -0400

    friends array

--- src/App.js
+++ src/App.js
@@ -3,32 +3,45 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
-import Nav from "./components/Nav"
+import Nav from "./components/Nav";
 
+function randomFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 class App extends Component {
   state = {
-    friends
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    correctIncorrect: "",
+    Clicked: []
   };
   render() {
     return (
       <Wrapper>
-      <Nav />
-        <Title>Clicky Game!
-        </Title>
-          {this.state.friends.map(friend => (
-              <FriendCard
-                key={friend.id}
-                handleClick={this.handleClick}
-                handleIncrement={this.handleIncrement}
-                handleReset={this.handleReset}
-                handleShuffle={this.handleShuffle}
-                id={friend.id}
-                image={friend.image}
-              />
-          ))}
+        <Nav />
+        <Title>Clicky Game!</Title>
+
+        {this.state.friends.map(friend => (
+          <FriendCard
+            key={friend.id}
+            handleClick={this.handleClick}
+            handleIncrement={this.handleIncrement}
+            handleReset={this.handleReset}
+            handleShuffle={this.handleShuffle}
+            id={friend.id}
+            image={friend.image}
+          />
+        ))}
       </Wrapper>
     );
   }
+
+
 }
 
 export default App;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,13 +6,6 @@ const Nav = props => (
     <h1>
       Click on an image to earn points, but don't click on any more than once !
     </h1>
-
-    <ul>
-      <li className="clicky" />
-      <li className="alignRight">
-        Score - Current: {props.topScore} | TOP: {props.score}
-      </li>
-    </ul>
   </nav>
 );
 
